a9492ec1c0c1a83b46922a6777f7a59501f6aa66
Automated build <runner@github.com>
2021-07-27 20:08:49 +0000

    Merge pull request #52 from p3palazzo/dev
    Tags novas na bibliografia, Tema Minimal-Mistakes + Tufte

---
+++ Makefile
@@ -0,0 +1,47 @@
+# {{{1 Variables
+#      =========
+
+VPATH = . assets
+vpath %.bib _bibliography
+vpath %.html . _includes _layouts _site
+vpath %.scss _sass assets/css assets/css-slides \
+	slides/reveal.js/css/theme/template
+vpath %.yaml . _spec
+
+PANDOC_VERSION  := 2.14
+JEKYLL_VERSION  := 4.2.0
+PANDOC/CROSSREF := docker run --rm -v "`pwd`:/data" \
+	-u "`id -u`:`id -g`" pandoc/crossref:$(PANDOC_VERSION)
+JEKYLL := palazzo/jekyll-tufte:$(JEKYLL_VERSION)-$(PANDOC_VERSION)
+
+ASSETS  = $(wildcard assets/*)
+AULA    = $(wildcard _aula/*.md)
+SLIDES  = $(patsubst _aula/%.md,slides/%/index.html,$(AULA))
+SASS    = revealjs.scss _revealjs-settings.scss \
+					mixins.scss settings.scss theme.scss
+
+# {{{1 Recipes
+#      =======
+.PHONY : _site
+_site : $(SLIDES)
+	@docker run --rm -v "`pwd`:/srv/jekyll" \
+		$(JEKYLL) /bin/bash -c "chmod 777 /srv/jekyll && jekyll build --future"
+
+slides/%/index.html : _aula/%.md revealjs.yaml \
+	revealjs-crossref.yaml references.bib $(SASS)
+	@-mkdir -p $(@D)
+	@$(PANDOC/CROSSREF) -o $@ -d _spec/revealjs.yaml $<
+	@echo $(@D)
+
+.PHONY : serve
+serve : $(SLIDES)
+	@docker run --rm -v "`pwd`:/srv/jekyll" \
+		-h "0.0.0.0:127.0.0.1" -p "4000:4000" \
+		$(JEKYLL) jekyll serve --future
+
+.PHONY : clean
+clean :
+	-@rm -rf *.aux *.bbl *.bcf *.blg *.fdb_latexmk *.fls *.log *.run.xml \
+		tau0005-*.tex
+
+# vim: set foldmethod=marker shiftwidth=2 tabstop=2 :
---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,187 @@
+var store = [{
+        "title": "Processos da arquitetura",
+        "excerpt":"Ordem e lugar A arquitetura organiza o ambiente humano por meio da construção. Essa organização não tem a ver com impor uma rigidez geométrica, mas com os princípios de ordem emergente que atendem às expectativas da percepção do ser humano e da socialização nas diferentes culturas. Esse é um processo...","categories": [],
+        "tags": [],
+        "url": "/tau0005/aula/processos/",
+        "teaser": null
+      },{
+        "title": "História e arquitetura tradicional",
+        "excerpt":"História ou arquitetura Em 1839, o arquiteto americano Ithiel Town encomendou ao seu amigo Thomas Cole uma pequena pintura de paisagem com algumas construções clássicas. Em vez disso, Cole pintou uma imensa alegoria da Arquitetura, intitulada Sonho do arquiteto (fig. ¿fig:cole-dream?). A tela representava os principais estilos arquitetônicos do Ocidente, desde...","categories": [],
+        "tags": [],
+        "url": "/tau0005/aula/arqtrad/",
+        "teaser": null
+      },{
+        "title": "Taipas e fundações",
+        "excerpt":"O contato com a Natureza justifica a racionalidade da técnica em particular e a autenticidade das práticas sociais em geral, como na cena pastoral da nossa já conhecida série de pinturas de Thomas Cole (fig. 1) que trata do Decurso do império. O monumento megalítico no promontório reúne duas credenciais que...","categories": [],
+        "tags": [],
+        "url": "/tau0005/aula/taipa/",
+        "teaser": null
+      },{
+        "title": "Construção arquitravada",
+        "excerpt":"Da forquilha ao pórtico Nos mitos da cabana primitiva e nas culturas da maior parte do mundo, a madeira é o material primordial da arquitetura. A construção tradicional em madeira é uma concepção aditiva, isto é, ela se caracteriza estruturalmente e visualmente pela montagem de peças claramente distintas entre si....","categories": [],
+        "tags": [],
+        "url": "/tau0005/aula/arquitravada/",
+        "teaser": null
+      },{
+        "title": "Construção arqueada",
+        "excerpt":"Tecnologia e profissão Figura 1: Distribuição de empuxos no corte de uma igreja gótica. Desenho por Jean-Claude Golvin Figura 2: Esquemas de arcos e abóbadas Figura 3: Dimensionamento dos contrafortes para diferentes tipos de arcos, segundo François Blondel, 1675 Figura 4: Cimbramento para uma abóbada de berço segundo Viollet-le-Duc, 1856...","categories": [],
+        "tags": [],
+        "url": "/tau0005/aula/arcos/",
+        "teaser": null
+      },{
+        "title": "Construção murária",
+        "excerpt":"Figura 1: Giovanni Paolo Pannini, Galeria de vistas da Roma antiga, 1757 Energia e arquitetura Construir com blocos A construção murária, ou seja, baseada em paredes, é a delimitação por excelência do espaço. Na seção anterior, vimos como a construção arquitravada vem do uso da madeira, e resulta em malhas...","categories": [],
+        "tags": [],
+        "url": "/tau0005/aula/muraria/",
+        "teaser": null
+      },{
+        "title": "Coberturas e superfícies",
+        "excerpt":"Estilo, construção e cultura Figura 1: Área de influência cultural chinesa no século VII d.C. Desenho: Brian Gotts, 2007 O mundo influenciado pela cultura chinesa se estendia, no seu auge durante o século VII d.C. (fig. 1), do Turquestão ao Japão e da Manchúria à Indochina.Liang, A Pictorial History of Chinese...","categories": [],
+        "tags": [],
+        "url": "/tau0005/aula/coberturas/",
+        "teaser": null
+      },{
+        "title": "Corpo e natureza",
+        "excerpt":"Figura 1: Edwin Lord Weeks. Across the Pool to the Golden Temple of Amritsar, 1883 Unidade na diversidade A cultura tradicional da Índia é exótica aos olhos do Ocidente num nível que outras culturas da Ásia não atingem. A China é centralizada, burocrática e materialista — todos valores que os...","categories": [],
+        "tags": [],
+        "url": "/tau0005/aula/clima/",
+        "teaser": null
+      },{
+        "title": "Propósitos",
+        "excerpt":"Figura 1: Edwin Lord Weeks, A Court in the Alhambra in the Time of the Moors Cultura doméstica no Mediterrâneo Figura 2: Mediterrâneo ocidental na época romana Figura 3: Sítio arqueológico de Lattara, feitoria etrusca no sul da Gália, séculos VII a.C.—III d.C. Desenho: Gailledrat e Vacheret, 2020 Figura 4:...","categories": [],
+        "tags": [],
+        "url": "/tau0005/aula/propositos/",
+        "teaser": null
+      },{
+        "title": "Volumes",
+        "excerpt":"Espaço ou lugar Do monumento maciço ao espaço escavado Figura 1: Reconstituição do Templo 40 em Sāñcī, segundo Percy Brown, 1959 Figura 2: Portal da caverna de Lomas Ṛṣi, Bihar, 250–185 a.C. Foto: Photo Dharma, 2013 Figura 3: Volumetria da caverna de Lomas Ṛṣi. Desenho: Percy Brown, 1959 Templo (pura)...","categories": [],
+        "tags": [],
+        "url": "/tau0005/aula/volumes/",
+        "teaser": null
+      },{
+        "title": "Teoria da forma urbana",
+        "excerpt":"Princípios da forma urbana Lugar tem precedência sobre o tempo Parataxis Res publica e res economica Transect Clássico e vernáculo Figura 1: Léon Krier, Civitas, 1983 Figura 2: Transecto urbano, por Duany, Plater-Zyberk &amp; Co. Figura 3: Transecto natural, por DPZ Figura 4: Transecto urbano, baseado em DPZ Arquitetura urbana...","categories": [],
+        "tags": [],
+        "url": "/tau0005/aula/comunidade/",
+        "teaser": null
+      },{
+        "title": "Completar",
+        "excerpt":"Quando termina a Antiguidade? Figura 1: Thomas Cole, The Course of Empire: Desolation, 1836 Figura 2: Palácio do imperador Diocleciano em Spalato, atual Split, Croácia, c. 306 d.C. Desenho: Ernest Hébrard, 1912 a b Figura 3: Espalato, plantas reconstituídas por Ernest Hébrard, 1912. a – Palácio de Diocleciano, b – Split, cidade construída sobre os...","categories": [],
+        "tags": [],
+        "url": "/tau0005/aula/completar/",
+        "teaser": null
+      },{
+        "title": "Ordenar",
+        "excerpt":"Traçado regulador e cosmologia no vāstu śāstra Figura 1: Reconstituição do assentamento de Arkaim, sul dos montes Urais, c. 2000 a.C. Desenho: Jvtrplzz Figura 2: Esquemas conceituais de aldeias indo-arianas. Desenho em E.B. Havell, 1918 Figura 3: Aldeia védica com cerca e portal. Desenho em Percy Brown, 1959 Do texto à...","categories": [],
+        "tags": [],
+        "url": "/tau0005/aula/ordenar/",
+        "teaser": null
+      },{
+        "title": "Articular",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/tau0005/aula/articular/",
+        "teaser": null
+      },{
+        "title": "Caracterizar",
+        "excerpt":"Figura 1: Thomas Cole. The Past, 1838 Casas, castelos e outras arquiteturas civis Figura 2: Villa parcialmente fortificada na época romana tardia, séculos IV–VI Figura 3: Tipos de base e duplicações na edilícia de base do Mediterrâneo medieval, segundo Caniggia Figura 4: Casa–salão do norte da Europa: construção em Burford,...","categories": [],
+        "tags": [],
+        "url": "/tau0005/aula/caracterizar/",
+        "teaser": null
+      },{
+        "title": "Proporcionar",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/tau0005/aula/proporcionar/",
+        "teaser": null
+      },{
+        "title": "Por que estudar história da arquitetura",
+        "excerpt":"Por que estudar arquitetura tradicional em pleno século XXI? A resposta a essa pergunta muda sem parar desde que a História começou a ser ensinada nos cursos de Arquitetura, uns 200 anos atrás. Para o historiador francês Ivan Jablonka, “o passado é um país estrangeiro”. O que se estuda como...","categories": [],
+        "tags": [],
+        "url": "/tau0005/plano/por-que-historia/",
+        "teaser": null
+      },{
+        "title": "Sobre a disciplina",
+        "excerpt":"TAU 0005 é a segunda disciplina na cadeia de Teoria e História do curso de graduação em Arquitetura e Urbanismo na Universidade de Brasília, e a primeira a se dedicar inteiramente ao estudo do passado. Ela é tanto uma cadeira de formação profissional quanto uma disciplina de conhecimentos gerais. A partir...","categories": [],
+        "tags": [],
+        "url": "/tau0005/plano/sobre-disciplina/",
+        "teaser": null
+      },{
+        "title": "O que você vai aprender",
+        "excerpt":"O objetivo da disciplina é conferir um domínio operativo da arquitetura tradicional em geral, e da linguagem clássica em particular, enquanto ferramentas de análise e produção sustentável e resiliente do ambiente construído. Este propósito se desdobra em três aspectos que correspondem a cada uma das unidades do Programa: Introdução à...","categories": [],
+        "tags": [],
+        "url": "/tau0005/plano/objetivos/",
+        "teaser": null
+      },{
+        "title": "Como vamos trabalhar",
+        "excerpt":"Nossas ferramentas Moodle Neste semestre, você vai continuar estudando em modo remoto. Todo o conteúdo e as atividades da disciplina estão no ambiente virtual de aprendizagem Aprender 3. Esse ambiente usa um sistema livre de ensino a distância chamado Moodle (pronuncia-se “múdou”). Por isso, na maioria das vezes vamos nos referir...","categories": [],
+        "tags": [],
+        "url": "/tau0005/plano/metodologia/",
+        "teaser": null
+      },{
+        "title": "Apoio ao aluno",
+        "excerpt":"O conteúdo e as atividades da disciplina estão disponíveis no ambiente virtual de aprendizagem Aprender 3. A equipe de TAU 0005 — o professor, a estagiária docente e as monitoras — estará disponível ao vivo, por videoconferência no sistema Microsoft Teams da UnB, às terças e quintas-feiras no horário da aula....","categories": [],
+        "tags": [],
+        "url": "/tau0005/plano/apoio/",
+        "teaser": null
+      },{
+        "title": "Avaliação",
+        "excerpt":"Nesta disciplina, você vai trabalhar em ritmo semanal. Cada semana corresponde a um tópico do conteúdo e pede a realização de uma tarefa assíncrona (ou seja, sem hora marcada), que conta como presença. Essas atividades semanais contribuem para a atividade avaliativa de cada Unidade, que vale nota. Para obter aprovação...","categories": [],
+        "tags": [],
+        "url": "/tau0005/plano/avalia/",
+        "teaser": null
+      },{
+        "title": "Leitura e estudo",
+        "excerpt":"Como estamos trabalhando a distância, a leitura e o estudo individual são componentes centrais da nossa disciplina. Não espere que a videoconferência vá suprir todo o conteúdo do qual você precisa para adquirir as competências e realizar as atividades! Antes, a videoconferência é uma ocasião para esclarecer dúvidas sobre pontos...","categories": [],
+        "tags": [],
+        "url": "/tau0005/plano/bibliografia/",
+        "teaser": null
+      },{
+        "title": "Composição de espaços",
+        "excerpt":"Ementa O espaço singular da edícula é o elemento de base da composição. A articulação entre espaços é a linguagem da composição arquitetônica. A relação entre várias composições é a “conversa” que forma as cidades. Para termos um diálogo produtivo no espaço urbano, é preciso que todos falem a mesma...","categories": [],
+        "tags": [],
+        "url": "/tau0005/trabalho/processo/",
+        "teaser": null
+      },{
+        "title": "Edículas vernáculas",
+        "excerpt":"Ementa Os sistemas construtivos tradicionais de todos os povos do mundo usam materiais predominantemente locais, pouco processados, e saberes empíricos lentamente refinados, privilegiando estruturas portantes à compressão. Esse processo resulta em arquiteturas com marcada identidade local. Dentro de uma ampla gama de variações regionais e culturais, a construção tradicional converge...","categories": [],
+        "tags": [],
+        "url": "/tau0005/trabalho/vernacular/",
+        "teaser": null
+      },{
+        "title": "Convenções para desenho de arquitetura tradicional",
+        "excerpt":"Observe as indicações escritas na prancha abaixo e também os modos de representação adotados. Figura 1: Modelo de prancha e convenções gráficas Desenhe usando os instrumentos e materiais indicados para esta disciplina e observe as dicas para digitalizar a sua entrega. Referências Ching, Francis D. K. Representação gráfica em arquitetura....","categories": [],
+        "tags": [],
+        "url": "/tau0005/trabalho/desenho/",
+        "teaser": null
+      },{
+        "title": "Como digitalizar os desenhos",
+        "excerpt":"Veja primeiro os instrumentos e materiais indicados para realizar as atividades, assim como as convenções de desenho que usamos. Para fazer as entregas, você precisa digitalizar os desenhos e carregá-los na respectiva atividade no Moodle. Se você não tiver acesso a um scanner tamanho A3 (quase ninguém tem, não se...","categories": [],
+        "tags": [],
+        "url": "/tau0005/trabalho/digitalizar/",
+        "teaser": null
+      },{
+        "title": "Instrumentos e materiais de desenho",
+        "excerpt":"Para fazer as atividades da disciplina, você vai precisar de um conjunto básico de instrumentos de desenho arquitetônico e de papel. Uma superfície lisa e macia para desenhar: prancheta portátil ou de pé, mesa de jantar ou tábua em MDF com bordas retas (para poder apoiar a régua “T” nas...","categories": [],
+        "tags": [],
+        "url": "/tau0005/trabalho/materiais/",
+        "teaser": null
+      },{
+        "title": "Estudo analítico: a cabana primitiva",
+        "excerpt":"[O] propósito [do estudo analítico] é garantir ao estudante, antes de ingressar no estudo da arquitetura propriamente dita, o comando de certo conhecimento das coisas necessárias para representar adequadamente uma edificação proposta. Lloyd Warren, Prefácio in John Harbeson, The Study of Architectural Design, 1927, p. 6. Objetivo Dominar algumas convenções fundamentais...","categories": [],
+        "tags": [],
+        "url": "/tau0005/trabalho/cabana/",
+        "teaser": null
+      },{
+        "title": "Composição com volumes",
+        "excerpt":"Ementa O volume singular da edícula é o elemento de base da arquitetura. A articulação entre volumes é a linguagem da composição arquitetônica. Numa composição arquitetônica tradicional, cada volume é claramente delimitado, tem uma forma geométrica clara, e é articulado de modo autônomo. Ao mesmo tempo, todos os volumes de...","categories": [],
+        "tags": [],
+        "url": "/tau0005/trabalho/volumes/",
+        "teaser": null
+      },{
+        "title": "Analítico com ordens clássicas",
+        "excerpt":"Projetar é compor, mas para compor é preciso ter objetos a serem compostos: para os arquitetos, estes são os “elementos da arquitetura”. Harbeson, The study of architectural design, 1926, p. 27. Ementa O estudo analítico tem sido a porta de entrada no estudo do classicismo desde a organização do ensino na...","categories": [],
+        "tags": [],
+        "url": "/tau0005/trabalho/classico/",
+        "teaser": null
+      }]
