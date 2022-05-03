25854a8f9bdc3d8f61245d87433e8e9b8b3c38f9
Automated build <runner@github.com>
2022-05-03 18:17:45 +0000

    Urbanizar (#130)
    * Reformata slides de Urbanizar
    
    * Correções nos slides

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -60,7 +60,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Urbanizar",
-        "excerpt":"Princípios da forma urbana Tradição e modernidade Mesmo nas construções mais modernas e industrializadas, processos tradicionais de urbanização continuam vigentes, tanto em áreas mais formalizadas, quanto nas mais vernaculares e tradicionais. Dinâmica urbana: as transformações da cidade, geralmente, contam com processos e ações que seguem lógicas tradicionais. Ressalta-se que processo...","categories": [],
+        "excerpt":"Princípios da forma urbana Tradição e modernidade Mesmo nas construções mais modernas e industrializadas, processos tradicionais de urbanização continuam vigentes, tanto em áreas mais formalizadas, quanto nas mais vernaculares e tradicionais. Dinâmica urbana as transformações da cidade, geralmente, contam com processos e ações que seguem lógicas tradicionais. Ressalta-se que processo...","categories": [],
         "tags": [],
         "url": "/tau0005/aula/urbanismo/",
         "teaser": null
