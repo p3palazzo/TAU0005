57426847b9c534304ee7aa720e7cc4c25c41d85e
Automated build <runner@github.com>
2022-12-20 19:05:59 +0000

    Atualiza URL com maiúsculas

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,162 +2,162 @@ var store = [{
         "title": "História e arquitetura tradicional",
         "excerpt":"História ou arquitetura Em 1839, o arquiteto americano Ithiel Town encomendou ao seu amigo Thomas Cole uma pequena pintura de paisagem com algumas construções clássicas. Em vez disso, Cole pintou uma imensa alegoria da Arquitetura, intitulada Sonho do arquiteto (fig. 1). A tela representava os principais estilos arquitetônicos do Ocidente, desde...","categories": [],
         "tags": [],
-        "url": "/tau0005/aula/arqtrad/",
+        "url": "/TAU0005/aula/arqtrad/",
         "teaser": null
       },{
         "title": "Processos da arquitetura",
         "excerpt":"Ordem e lugar A arquitetura organiza o ambiente humano por meio da construção. Essa organização não tem a ver com impor uma rigidez geométrica, mas com os princípios de ordem emergente que atendem às expectativas da percepção do ser humano e da socialização nas diferentes culturas. Esse é um processo...","categories": [],
         "tags": [],
-        "url": "/tau0005/aula/processos/",
+        "url": "/TAU0005/aula/processos/",
         "teaser": null
       },{
         "title": "Taipas e fundações",
         "excerpt":"Materiais da construção tradicional Os mitos de origem exercem uma atração especial sobre as teorias da arquitetura. A visão de mundo na qual os primórdios de algum modo explicam, e por que não justificam, o desenvolvimento histórico da cultura é recorrente não só em sociedades tradicionais, mas inclusive em movimentos...","categories": [],
         "tags": [],
-        "url": "/tau0005/aula/taipa/",
+        "url": "/TAU0005/aula/taipa/",
         "teaser": null
       },{
         "title": "Construção arquitravada",
         "excerpt":"Tendas e cabanas Nos mitos da cabana primitiva e nas culturas da maior parte do mundo, a madeira é o material primordial da arquitetura. A construção tradicional em madeira é uma concepção aditiva, isto é, ela se caracteriza estruturalmente e visualmente pela montagem de peças claramente distintas entre si. Quase...","categories": [],
         "tags": [],
-        "url": "/tau0005/aula/arquitravada/",
+        "url": "/TAU0005/aula/arquitravada/",
         "teaser": null
       },{
         "title": "Construção murária",
         "excerpt":"Princípios da construção murária A construção murária resulta em estruturas que geralmente têm uma aparência de grandes massas sólidas, e que delimitam com muita clareza os espaços arquitetônicos. A taipa e a alvenaria são os principais sistemas de construção tradicional de paredes portantes. Eles consistem num uso mais ou menos...","categories": [],
         "tags": [],
-        "url": "/tau0005/aula/muraria/",
+        "url": "/TAU0005/aula/muraria/",
         "teaser": null
       },{
         "title": "Coberturas e superfícies",
         "excerpt":"Função e estrutura das coberturas Nem tudo o que a arquitetura produz é espaço coberto, mas cobrir ambientes é uma das tarefas mais diretamente associadas com a arquitetura e a construção. As coberturas não só protegem quem está dentro de uma edificação, mas sobretudo protegem a própria edificação contra a...","categories": [],
         "tags": [],
-        "url": "/tau0005/aula/coberturas/",
+        "url": "/TAU0005/aula/coberturas/",
         "teaser": null
       },{
         "title": "Construção arqueada",
         "excerpt":"Geometria e forças As estruturas tradicionais em alvenaria estrutural sustentam a construção ao mesmo tempo que elas delimitam espaços interiores. No entanto, essas paredes só são úteis se pudermos praticar aberturas nelas para permitir a passagem, iluminação e ventilação. Assim como a concepção estrutural do conjunto, essas aberturas podem ser...","categories": [],
         "tags": [],
-        "url": "/tau0005/aula/arcos/",
+        "url": "/TAU0005/aula/arcos/",
         "teaser": null
       },{
         "title": "Ambiente",
         "excerpt":"Clima como variável independente das culturas No registro das tradições arquitetônicas ao longo do tempo, a geologia e o clima nas diferentes partes do mundo determinam a maior parte dos materiais de construção que podem ser usados num certo lugar. Já numa perspectiva contemporânea, onde as cadeias produtivas podem ser...","categories": [],
         "tags": [],
-        "url": "/tau0005/aula/ambiente/",
+        "url": "/TAU0005/aula/ambiente/",
         "teaser": null
       },{
         "title": "Propósito",
         "excerpt":"Programa, função e propósito Morfologia e tipologia Revisão crítica do projeto modernista Figura 1: Le Corbusier, arquiteto, maquete do plano Voisin com a sua inserção no centro histórico de Paris (1925) Figura 2: West End, Boston, demolição do bairro para construção do novo centro administrativo. Foto: Lawrence Lowry, 1959 Figura...","categories": [],
         "tags": [],
-        "url": "/tau0005/aula/proposito/",
+        "url": "/TAU0005/aula/proposito/",
         "teaser": null
       },{
         "title": "Espaço",
         "excerpt":"Espaço ou vazio Uma das principais diferenças entre a arquitetura tradicional e a moderna se refere aos processos construtivos, sua industrialização e consumo de uma quantidade muito maior de energia (no segundo caso). Isso faz com que esses processos se tornem deslocalizados e com cadeias produtivas extremamente profundas e concentradas...","categories": [],
         "tags": [],
-        "url": "/tau0005/aula/espaco/",
+        "url": "/TAU0005/aula/espaco/",
         "teaser": null
       },{
         "title": "Proporção e escala",
         "excerpt":"Proporção e estética Figura 1: Princípios vitruvianos Por que proporções estão sendo tratadas na parte do curso que se dedica aos elementos de composição, e não naquela que trata das teorias da arquitetura? É um equívoco acreditarmos que a “boa forma” de uma construção seja um resultado estético que precisa...","categories": [],
         "tags": [],
-        "url": "/tau0005/aula/proporcionar/",
+        "url": "/TAU0005/aula/proporcionar/",
         "teaser": null
       },{
         "title": "Ordenar",
         "excerpt":"Propriedades arquitetônicas Segundo Christopher Alexander.The Nature of Order. Níveis de escala Escalas sucessivas com menos de uma ordem de grandeza entre elas. Padrões fortes Desenvolvimento do conceito avançado no livro Linguagem de padrõesAlexander et al., A Pattern Language. para uma avaliação não determinista. Contorno O contorno é um elemento de...","categories": [],
         "tags": [],
-        "url": "/tau0005/aula/ordenar/",
+        "url": "/TAU0005/aula/ordenar/",
         "teaser": null
       },{
         "title": "Articular",
         "excerpt":"Procedimentos arquitetônicos Fractal Repetições de elementos estruturalmente semelhantes, mas não idênticos; Passo a passo generativo; Preserva a estrutura dos estágios anteriores; Quando necessário, replica e colapsa a ideia da passagem do tempo num projeto; Linguagem de padrões é materialista: importa o efeito e o desempenho, não a justificativa teórica; Planejamento...","categories": [],
         "tags": [],
-        "url": "/tau0005/aula/articular/",
+        "url": "/TAU0005/aula/articular/",
         "teaser": null
       },{
         "title": "Caracterizar",
         "excerpt":"Cultura arquitetônica A cultura arquitetônica é composta por três categorias de elementos ou condicionantes: Literacia Conhecimentos e repertórios existentes numa certa região ou dominados por uma certa categoria profissional, ou por leigos. Este é o nível mais básico que informa o universo de possibilidades da arquitetura. Decoro Convenções sociais e...","categories": [],
         "tags": [],
-        "url": "/tau0005/aula/caracterizar/",
+        "url": "/TAU0005/aula/caracterizar/",
         "teaser": null
       },{
         "title": "Urbanizar",
         "excerpt":"Princípios da forma urbana Tradição e modernidade Mesmo nas construções mais modernas e industrializadas, processos tradicionais de urbanização continuam vigentes, tanto em áreas mais formalizadas, quanto nas mais vernaculares e tradicionais. Dinâmica urbana as transformações da cidade, geralmente, contam com processos e ações que seguem lógicas tradicionais. Ressalta-se que processo...","categories": [],
         "tags": [],
-        "url": "/tau0005/aula/urbanismo/",
+        "url": "/TAU0005/aula/urbanismo/",
         "teaser": null
       },{
         "title": "Por que estudar história da arquitetura",
         "excerpt":"Por que estudar arquitetura tradicional em pleno século XXI? A resposta a essa pergunta muda sem parar desde que a História começou a ser ensinada nos cursos de Arquitetura, uns 200 anos atrás. Para o historiador francês Ivan Jablonka, “o passado é um país estrangeiro”. O que se estuda como...","categories": [],
         "tags": [],
-        "url": "/tau0005/plano/por-que-historia/",
+        "url": "/TAU0005/plano/por-que-historia/",
         "teaser": null
       },{
         "title": "Sobre a disciplina",
         "excerpt":"TAU 0005 é a segunda disciplina na cadeia de Teoria e História do curso de graduação em Arquitetura e Urbanismo na Universidade de Brasília, e a primeira a se dedicar inteiramente ao estudo do passado. Ela é tanto uma cadeira de formação profissional quanto uma disciplina de conhecimentos gerais. A partir...","categories": [],
         "tags": [],
-        "url": "/tau0005/plano/sobre-disciplina/",
+        "url": "/TAU0005/plano/sobre-disciplina/",
         "teaser": null
       },{
         "title": "O que você vai aprender",
         "excerpt":"Objetivo geral O objetivo da disciplina é conferir um domínio operativo da arquitetura tradicional em geral, e da linguagem clássica em particular, enquanto ferramentas de análise e produção sustentável e resiliente do ambiente construído. Este propósito se desdobra em quatro aspectos que correspondem a cada uma das unidades do Programa:...","categories": [],
         "tags": [],
-        "url": "/tau0005/plano/objetivos/",
+        "url": "/TAU0005/plano/objetivos/",
         "teaser": null
       },{
         "title": "Metodologia e recursos de apoio",
         "excerpt":"Moodle Todo o conteúdo e as ementas dos trabalhos da disciplina estão no ambiente virtual de aprendizagem Aprender 3. Esse ambiente usa um sistema livre de ensino a distância chamado Moodle (pronuncia-se “múdou”). Por isso, na maioria das vezes vamos nos referir à plataforma virtual como o Moodle. O login do...","categories": [],
         "tags": [],
-        "url": "/tau0005/plano/metodologia/",
+        "url": "/TAU0005/plano/metodologia/",
         "teaser": null
       },{
         "title": "Avaliação",
         "excerpt":"Critérios para aprovação na disciplina O que esperamos de todas as entregas é atenção ao que foi pedido, pesquisa e leitura criteriosa do material necessário à realização do trabalho, reflexão crítica, clareza e capricho na execução. Para obter aprovação na disciplina, você precisa atender a todos os três requisitos seguintes:...","categories": [],
         "tags": [],
-        "url": "/tau0005/plano/avalia/",
+        "url": "/TAU0005/plano/avalia/",
         "teaser": null
       },{
         "title": "Leitura e estudo",
         "excerpt":"A leitura e o estudo individual são componentes centrais da nossa disciplina. Não espere que a aula expositiva vá suprir todo o conteúdo do qual você precisa para adquirir as competências e realizar os trabalhos! Antes, o encontro em aula é uma ocasião para esclarecer dúvidas sobre pontos do conteúdo...","categories": [],
         "tags": [],
-        "url": "/tau0005/plano/bibliografia/",
+        "url": "/TAU0005/plano/bibliografia/",
         "teaser": null
       },{
         "title": "Estudo analítico: a cabana primitiva",
         "excerpt":"[O] propósito [do estudo analítico] é garantir ao estudante, antes de ingressar no estudo da arquitetura propriamente dita, o comando de certo conhecimento das coisas necessárias para representar adequadamente uma edificação proposta. Lloyd Warren, Prefácio in John Harbeson, The Study of Architectural Design, 1927, p. 6. Objetivo Dominar algumas convenções fundamentais...","categories": [],
         "tags": [],
-        "url": "/tau0005/trabalho/cabana/",
+        "url": "/TAU0005/trabalho/cabana/",
         "teaser": null
       },{
         "title": "Edículas vernáculas",
         "excerpt":"Ementa Os sistemas construtivos tradicionais de todos os povos do mundo usam materiais predominantemente locais, pouco processados, e saberes empíricos lentamente refinados, privilegiando estruturas portantes à compressão. Esse processo resulta em arquiteturas com marcada identidade local. Dentro de uma ampla gama de variações regionais e culturais, a construção tradicional converge...","categories": [],
         "tags": [],
-        "url": "/tau0005/trabalho/vernacular/",
+        "url": "/TAU0005/trabalho/vernacular/",
         "teaser": null
       },{
         "title": "Composição espacial",
         "excerpt":"Ementa Figura 1: Cesare Cesariano, composição modular em quina, da tradução do Tratado de arquitetura de Vitrúvio, 1521 O volume singular da edícula é o elemento de base da arquitetura. A articulação entre volumes é a linguagem da composição arquitetônica. Numa composição arquitetônica tradicional, cada volume é claramente delimitado, tem...","categories": [],
         "tags": [],
-        "url": "/tau0005/trabalho/volumes/",
+        "url": "/TAU0005/trabalho/volumes/",
         "teaser": null
       },{
         "title": "Analítico com ordens clássicas",
         "excerpt":"Projetar é compor, mas para compor é preciso ter objetos a serem compostos: para os arquitetos, estes são os “elementos da arquitetura”. Harbeson, The study of architectural design, 1926, p. 27. Ementa Figura 1: Léon Krier, Elementos da construção vernácula e elementos da arquitetura clássica O estudo analítico tem sido a...","categories": [],
         "tags": [],
-        "url": "/tau0005/trabalho/classico/",
+        "url": "/TAU0005/trabalho/classico/",
         "teaser": null
       },{
         "title": "Instrumentos e materiais",
         "excerpt":"Para fazer as atividades da disciplina, você vai precisar de um conjunto básico de instrumentos de desenho arquitetônico e modelagem, seja à mão ou digital. Desenho à mão Materiais indicados Uma superfície lisa e macia para desenhar: prancheta portátil ou de pé, mesa de jantar ou tábua em MDF com...","categories": [],
         "tags": [],
-        "url": "/tau0005/trabalho/materiais/",
+        "url": "/TAU0005/trabalho/materiais/",
         "teaser": null
       },{
         "title": "Composição de espaços",
         "excerpt":"Ementa O espaço singular da edícula é o elemento de base da composição. A articulação entre espaços é a linguagem da composição arquitetônica. A relação entre várias composições é a “conversa” que forma as cidades. Para termos um diálogo produtivo no espaço urbano, é preciso que todos falem a mesma...","categories": [],
         "tags": [],
-        "url": "/tau0005/trabalho/processo/",
+        "url": "/TAU0005/trabalho/processo/",
         "teaser": null
       }]
